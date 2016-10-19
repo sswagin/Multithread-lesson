dfd44206d1df66ef42ba36a15dacd94ee748861d
sswagin <sswagin@gmail.com>
2016-10-19 19:34:14 +0300

    test commit

--- src/TestConvinient/Main.java
+++ src/TestConvinient/Main.java
@@ -4,4 +4,9 @@ package TestConvinient;
  * Created by Smaug on 19.10.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+        int i = 0; //initialization of variable
+        
+
+    }
 }
